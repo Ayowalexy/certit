13c815b6dd39a81873d1d282937456793a5b90ee
Ayowalexy <shalom.mathew@wayapaychat.com>
2022-12-11 12:51:55 +0100

    fixes

--- src/components/Testimonials/index.js
+++ src/components/Testimonials/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import './testimonials.styles.css';
 
-const Card = () => (
+const Card = ({name, text}) => (
     <div className="l_container">
         <div className="l_box" />
         <div className="short">
-            Short Courses Certifcation, Diploma, Higher Diploma, MSc, MBA, PHD in in-demand digital skills.
+            {name}
         </div>
         <div className="saying">
-            What Learners are<br /> saying
+           {text}
         </div>
     </div>
 )
@@ -21,11 +21,25 @@ const Testimonials = () => {
                 <div className="learner">
                     What Learners are saying
                 </div>
+                
 
                 <div className="text_box_cont">
-                    <Card />
-                    <Card />
-                    <Card />
+                   
+                    {
+                    [
+                        {
+                            name: 'I have been looking for an online platform to learn Data Analysis and I came to the right platform.',
+                            text: 'Opurum Merit'
+                        },{
+                            name: 'The self-paced lectures has been very helpful to my tight work schedule. I learn when I’m back from work',
+                            text: 'Daniel Ajayi'
+                        },{
+                            name: 'I’m looking forward to start my course scheduled for the new batch, I hope it will be mind  blowing.',
+                            text: 'Akere Kabari'
+                        },
+
+                    ].map((element, idx) => <Card name={element.name} text={element.text} key={idx} />)
+                }
                 </div>
             </div>
         </div>
